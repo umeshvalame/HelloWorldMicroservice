dc5d3bcbd524ce310c065f9db7ee8fdd90804a1c
umeshvalame <umesh.valame@gmail.com>
2024-10-02 12:05:53 -0400

    httpd server serving hello world html output when called

---
+++ scripts/httpd.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Use this for your user data (script from top to bottom)
+# install httpd (Linux 2 version)
+Yum install -y -httpd
+systemctl start httpd
+systemctl enable httpd
+echo “<h1> Hello World from ${hostname -f }</h1>” > /var/www/html/index.html 
