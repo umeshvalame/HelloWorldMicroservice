12730946f92eb57a05e28b694f01892d2b394681
umeshvalame <umesh.valame@gmail.com>
2024-10-02 12:10:21 -0400

    syntax error fixed for yum command

--- scripts/httpd.sh
+++ scripts/httpd.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Use this for your user data (script from top to bottom)
 # install httpd (Linux 2 version)
-Yum install -y -httpd
+yum update -y
+yum install -y -httpd
 systemctl start httpd
 systemctl enable httpd
 echo “<h1> Hello World from ${hostname -f }</h1>” > /var/www/html/index.html 
