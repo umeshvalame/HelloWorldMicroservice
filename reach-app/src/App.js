7587821b20a415cad091f6f6d5475b6e4952c3e2
umeshvalame <umesh.valame@gmail.com>
2024-10-25 13:25:26 -0400

    About page updated

--- reach-app/src/App.js
+++ reach-app/src/App.js
@@ -4,25 +4,48 @@ import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import EmployeeForm from './EmployeeForm';
 
-function DisplayData({name, identification}) {
+function DisplayAboutData(full_name) {
+  return (
+    <div>
+      <nav>
+        <table border={1}>
+          <tr>
+            <td><Link to="/">Home</Link></td> <td> / </td>
+            <td><Link to="/contact">Contact</Link></td>
+          </tr>
+        </table>
+      </nav>
+      <h3>
+        User Rank : {full_name}
+      </h3>
+    </div>
+    
+  );
+}
+
+function DisplayData({profile_url, identification, updated_at}) {
 
   return (
     <div>
       <nav>
-      <table>
+      <table border={1}>
           <tr>
             <td>
-              <Link to="/about">About</Link> </td>
+              <Link to="/about">About</Link> </td><td> / </td>
               <td><Link to="/contact">Contact</Link></td>
             </tr>
       </table>      
       </nav>
-      <h1>
-        {name}
-      </h1>
-      <h2>
-        {identification}
-      </h2>
+      <h3>
+        GitHub repository URL : 
+        <b>{profile_url}</b>
+      </h3>
+      <h3>
+        User Rank : {identification}
+      </h3>
+      <h3>
+        Updated at : {updated_at}
+      </h3>
     </div>
   );
 
@@ -32,33 +55,51 @@ export function Contact() {
     return (
     <div>
       <nav>
-        <table>
+        <table border={1}>
           <tr>
-            <td><Link to="/">Home</Link></td>
+            <td><Link to="/">Home</Link></td> <td> / </td>
             <td><Link to="/about">About</Link></td>
           </tr>
         </table>
       </nav>
-      <h1>
-      YOU ARE IN Contact US page
-      </h1>
-      
+      <h3>
+      You are in Contact us page
+      </h3>
     </div>
   );
 }
 
 export function About() {
-  return (
-    <div>
-      <nav>
-      <Link to="/">Home</Link>
-      <Link to="/contact">Contact</Link>
-      </nav>
-      <h1>
-        You are in About US
-      </h1>
-    </div>
-  );
+
+    const [data,setData] = useState(null);
+    useEffect(() => {
+        fetch('https://api.github.com/users/umeshvalame/subscriptions')
+        .then((response) => response.json())
+        .then(setData);
+    },[]);
+    if(data) 
+      return (
+        <div>
+          <nav>
+            <table border={1}>
+              <tr>
+                <td><Link to="/">Home</Link></td> <td> / </td>
+                <td><Link to="/contact">Contact</Link></td>
+              </tr>
+            </table>
+          </nav>
+          {data.map((item) => (
+            <div key={item.id}> {/* Replace 'id' with the appropriate unique identifier */}
+              <p></p>
+              <b>Microservice :</b> {item.name}<p>
+              <b>Full Name : </b>{item.full_name}</p>
+              <p><b>Language : </b>{item.language}</p>
+              <p>Above ReactJs RestAPI is hosted on cloud 
+               <p></p> User -> ELB -> ECS(instance_type=fargage) -> c1(container task) -> DAX - DyanmoDB</p>
+            </div>
+          ))}
+        </div>
+      );
 }
 
 export function App() {
@@ -69,15 +110,7 @@ useEffect(() => {
       .then(setData);
 },[]);
 if(data) 
-  return <DisplayData name={data.login} identification={data.id} />
-
-  /*return (
-    <div className="App">
-      <h1>
-      <EmployeeForm />
-      </h1>
-    </div>
-  );*/
+  return <DisplayData profile_url={data.html_url} identification={data.id} updated_at={data.updated_at}/>
 }
 
 export default App;
